0328e2c9c5210218cc0ef9be675c904d83ef3918
mrudulamenon <mrudulammenon@gmail.com>
2020-06-07 15:06:29 +0530

    changing from promise to callback

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
-$(function(){
-    $("#login").click(function(){
-// function validate() {
-    // alert("validate");
-    var promise1 = new Promise(function (resolve, reject) {
-        let userid = $("#userid").val();
-        let password = $("#password").val();
-        console.log(userid,password);
+$(function () {
+    //using Promise
+
+    //     $("#login").click(function(){
+    // // function validate() {
+    //     // alert("validate");
+    //     var promise1 = new Promise(function (resolve, reject) {
+    //         let userid = $("#userid").val();
+    //         let password = $("#password").val();
+    //         console.log(userid,password);
+    //         if (userid === "admin" && password === "12345") {
+    //             resolve();
+    //         }
+    //         else{
+    //             reject();
+    //         }
+    //     });
+    //     // var result=await promise1;
+    //     promise1
+    //     .then(function(){$("#frm1").submit();})
+    //     .catch(function(){$("#error").html("Sorry!! Authentication Invalid!!");});
+    // });
+
+    //using callback
+
+    function redirect() {
+        $("#frm1").submit();
+    }
+    $("#login").click(function(){validate($("#userid").val(),$("#password").val(),redirect)});
+    function validate(userid,password,func) {
+        let id = userid;
+        let pwd = password;
+        console.log(id, pwd);
         if (userid === "admin" && password === "12345") {
-            resolve();
+            func();
         }
-        else{
-            reject();
+        else {
+            $("#error").html("Sorry!! Authentication Invalid!!");
         }
-    });
-    // var result=await promise1;
-    promise1
-    .then(function(){$("#frm1").submit();})
-    .catch(function(){$("#error").html("Sorry!! Authentication Invalid!!");});
-});
+    }
 });
